58d27fc08663a774745643a3b4ed08b4d487b9c0
gscott4 <grayson.scott@outlook.com>
2019-03-19 20:00:56 -0400

    Added CORS filter and linked two applications together

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'app';
+  constructor(private _http: HttpClient) {}
+
+  // Authentication that we need to send to OAuth.
+  getToken() {
+    this._http.post(
+      "http://localhost:8080/oauth/token?grant_type=password&username=Grayson&password=123",
+      {
+        withCredentials: true
+      },
+      {
+        headers: new HttpHeaders({
+          Authorization: "Basic " + btoa("client:secret")
+        })
+      }
+      // Log our response to the console
+    ).subscribe(res => {console.log(res);this.getUserNameOfToken(res)});
+  }
+
+  getUserNameOfToken(tokenInfo) {
+    this._http.post(
+      "http://localhost:8080/api/getUserName",
+      {
+        withCredentials: true
+      },
+      {
+        headers: new HttpHeaders({
+          Authorization: "Bearer " + tokenInfo['access_token']
+        })
+      }
+      // Log our response to the console
+    ).subscribe(res => console.log(res));
+  }
 }
